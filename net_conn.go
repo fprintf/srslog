f194d0ee15bb1c277bebf5354cf2f8122939cb6f
dead <dead@itinerantadmin.com>
2020-04-28 11:25:35 -0700

    Add a 30 second deadline on writes so they dont block forever

--- net_conn.go
+++ net_conn.go
@@ -2,6 +2,7 @@ package srslog
 
 import (
 	"net"
+	"time"
 )
 
 // netConn has an internal net.Conn and adheres to the serverConn interface,
@@ -20,6 +21,10 @@ func (n *netConn) writeString(framer Framer, formatter Formatter, p Priority, ho
 		formatter = DefaultFormatter
 	}
 	formattedMessage := framer(formatter(p, hostname, tag, msg))
+	// Set a 30 second write deadline on sending to syslog
+	if err := n.conn.SetWriteDeadline(time.Now().Add(30 * time.Second)); err != nil {
+		return err
+	}
 	_, err := n.conn.Write([]byte(formattedMessage))
 	return err
 }
